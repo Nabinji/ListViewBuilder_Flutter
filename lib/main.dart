6cbca39b4b78a18a394063675b465c4fdcf782d6
Nabin Paudyal <nabinpaudyal2057@gmail.com>
2023-08-06 00:17:51 +0545

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_listview/profile%20ui/profile.dart';
-// import 'my_home_page.dart';
+
+import 'my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyProfile(),
-      // MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
